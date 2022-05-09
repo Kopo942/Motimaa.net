fde4a268e0898f9ba971e3ac6f1da70f8e3f7bb0
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2022-05-09 12:42:39 +0300

    Remove unused div + styling import

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,10 @@
 import Navbar from "client/components/Navbar/Navbar";
-import styles from "client/styles/Home.module.css";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <>
       <Navbar />
-      <div className={styles.container}></div>
     </>
   );
 };
